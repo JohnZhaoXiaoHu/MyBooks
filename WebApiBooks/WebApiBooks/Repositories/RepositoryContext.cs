376e2c8716692aed3ce7cd6b9643d95d39a1e7ba
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-08-20 19:08:15 +0300

    Create RepositoryContext.cs
    create  fild DbContext

---
+++ WebApiBooks/WebApiBooks/Repositories/RepositoryContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiBooks.Models;
+
+namespace WebApiBooks.Repositories
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
