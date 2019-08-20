aeb139c25688c4a6e36998f4da78277efa5fbe95
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-08-20 17:56:03 +0300

    Create Repository.cs
    Add (CRUD) methods;

---
+++ WebApiBooks/WebApiBooks/RepopsitoryContracts/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiBooks.Models;
+
+namespace WebApiBooks.RepopsitoryContracts
+{
+   public interface IRepository
+    {
+        IEnumerable<Book> Books { get; }
+        Book this[int id] { get; }
+        Book AddBook(Book book);
+        Book Update(Book book);
+        void DeleteBook(int id);
+
+    }
+}
