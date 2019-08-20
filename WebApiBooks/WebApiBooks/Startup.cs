f7581415843f3e9f2a66a4c4230a816d12d00661
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-08-20 19:10:59 +0300

    Update Startup.cs
    declaration services DbContext;

--- WebApiBooks/WebApiBooks/Startup.cs
+++ WebApiBooks/WebApiBooks/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using WebApiBooks.Repositories;
 
 namespace WebApiBooks
 {
@@ -25,6 +27,7 @@ namespace WebApiBooks
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<RepositoryContext>(opts => opts.UseSqlServer(Configuration["ConnectionString:BookDB"]));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
