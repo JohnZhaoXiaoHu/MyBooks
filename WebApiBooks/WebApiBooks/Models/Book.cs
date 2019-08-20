6c5df1d03ccc1248d098d38a7451a7d97c3e4084
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-08-20 18:24:58 +0300

    Update Book.cs
    add fild and atribute;

--- WebApiBooks/WebApiBooks/Models/Book.cs
+++ WebApiBooks/WebApiBooks/Models/Book.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,10 @@ namespace WebApiBooks.Models
 {
     public class Book
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BookId { get; set; }
+
         public string nameBook { get; set; }
 
         public string Author { get; set; }
