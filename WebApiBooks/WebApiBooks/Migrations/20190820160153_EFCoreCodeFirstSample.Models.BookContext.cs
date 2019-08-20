720393f66e7cdf2bb3e176926894485710c3f025
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-08-20 19:11:55 +0300

    Used Migrations
    Create model ORM

---
+++ WebApiBooks/WebApiBooks/Migrations/20190820160153_EFCoreCodeFirstSample.Models.BookContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApiBooks.Migrations
+{
+    public partial class EFCoreCodeFirstSampleModelsBookContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "book",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    nameBook = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true),
+                    Page = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_book", x => x.BookId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "book");
+        }
+    }
+}
