d4b121da37bb92fe5cf73eab054786b0d4d30d23
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-08-20 20:50:28 +0300

    Fix Update IBookRepository<Book>
    fix methods;

--- WebApiBooks/WebApiBooks/RepopsitoryContracts/IBookRepository.cs
+++ WebApiBooks/WebApiBooks/RepopsitoryContracts/IBookRepository.cs
@@ -6,13 +6,12 @@ using WebApiBooks.Models;
 
 namespace WebApiBooks.RepopsitoryContracts
 {
-   public interface IBookRepository
-    { 
-        IEnumerable<Book> Books { get; }
-        Book this[int id] { get; }
-        Book AddBook(Book book);
-        Book Update(Book book);
-        void DeleteBook(int id);
-
+   public interface IBookRepository<Book>
+    {
+        IEnumerable<Book> GetAll();
+        Book Get(int id);
+        void Add(Book entity);
+        void Update(Book dbEntity, Book entity);
+        void Delete(Book entity);
     }
 }
