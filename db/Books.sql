26d0d0d10d45aea38e2ec2932f344c75453ce74a
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-08-20 21:57:45 +0300

    Add Book.sql

---
+++ db/Books.sql
@@ -0,0 +1,23 @@
+USE [BookDB]
+GO
+
+/****** Object:  Table [dbo].[book]    Script Date: 20.08.2019 21:54:55 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[book](
+	[BookId] [int] IDENTITY(1,1) NOT NULL,
+	[nameBook] [nvarchar](max) NULL,
+	[Author] [nvarchar](max) NULL,
+	[Page] [int] NOT NULL,
+ CONSTRAINT [PK_book] PRIMARY KEY CLUSTERED 
+(
+	[BookId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
