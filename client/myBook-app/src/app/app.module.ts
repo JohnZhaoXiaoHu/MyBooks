9b45d57bc1ce18de840ce3a6266e40bf88707abe
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-08-26 17:02:37 +0300

    Add show-bookComponent

--- client/myBook-app/src/app/app.module.ts
+++ client/myBook-app/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import { ShowBookComponent } from './book/show-book/show-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowBookComponent
   ],
   imports: [
     BrowserModule,
